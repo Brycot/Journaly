fd69820dddadffe92a3ca7a7906380546b8c471c
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-02-09 18:26:34 -0500

    [ADD/FIX] View Details in note view & fix bug with useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -40,6 +40,10 @@ export const useForm = (initialForm = {}, formValidations = {}) => {
         createValidators();
     }, [formState]);
 
+    useEffect(() => {
+        setFormState(initialForm);
+    }, [initialForm]);
+
     return {
         ...formState,
         formState,
